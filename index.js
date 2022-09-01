5481fdce2f4f26856bf0d3613904ebc05a6099a8
vedantpawar18 <vedantpawar18@gmail.com>
2022-09-01 17:00:24 +0530

    create delete read file using command line in nodejs

---
+++ index.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+
+
+const argvs = process.argv
+const argv = argvs.slice(2)
+const operation = argv[0]
+const operator1 = (argv[1])
+const operator2 = (argv[2])
+  
+
+
+ if (operation=="read")
+ {
+    fs.readFile(operator1, 'utf8',(err, data) => {
+        console.log(data);
+     })
+ }
+
+ if (operation=="create")
+ {
+    fs.appendFile(operator1, operator2, function (err) {
+        if (err) throw err;
+        console.log('File is created successfully.');
+      });
+
+ }
+ if (operation=="delete")
+ {
+    fs.unlink(operator1, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("File is deleted.");
+    });
+ }
